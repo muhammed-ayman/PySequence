031a7c556f22131b0c8e7a1c5e09821a008d8145
HeshamElsaman <hesham_ahmed_@outlook.com>
2021-08-19 04:25:12 +0200

    Update longestRepeat.py
    - Changed the variable name from 'num' to 'segment'
    - Repaired an error by changing another variable name from 'r' to 'start'

--- modules/longestRepeat.py
+++ modules/longestRepeat.py
@@ -3,19 +3,19 @@ def get_longest_consecutive_repeat_s_(seq):                 # getting a DNA/RNA 
     repeats = []                                            # an empty list to store the max-length repetitive segments
     max_ = 0                                                # the max length
     while start < len(seq):
-        num = ''                                            # an empty string to store a repetitive segment at a time
+        segment = ''                                        # an empty string to store a repetitive segment at a time
         for i in range(start, len(seq)):                    # looping the strand and recording all repetitive segments
             for j in range(start + 1, len(seq) + 1):
                 if j != len(seq) and seq[j] == seq[i]:
-                    num += seq[j]
+                    segment += seq[j]
                 else:
-                    r = j
-                    if len(num) > 0:
-                        if len(num) > max_:                 # if-elif statement to assure that only max-length repeats are stored
-                            max_ = len(num)
-                            repeats = [[num[0], i + 1]]
-                        elif len(num) == max_:
-                            repeats.append([num[0], i + 1])
+                    start = j
+                    if len(segment) > 0:
+                        if len(segment) > max_:             # if-elif statement to assure that only max-length repeats are stored
+                            max_ = len(segment)
+                            repeats = [[segment[0], i + 1]]
+                        elif len(segment) == max_:
+                            repeats.append([segment[0], i + 1])
                     break                                   # breaks once a repetitive segment ends
             break                                           # breaks and start searching again from where it stopped till it reaches the end of the strand
     if not repeats:
