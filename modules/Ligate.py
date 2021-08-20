38ae39bdc2caa3b61dc5250724ef68bbb96145b3
Ahmed Hesham Saadawy <44484663+ahmedhesham47@users.noreply.github.com>
2021-08-20 17:11:01 +0200

    Display last letter
    Edited the code so that it displays the LAST letter of the user's original sequence. It stops at len(sequence) rather than len(sequence) - 1 now.

--- modules/Ligate.py
+++ modules/Ligate.py
@@ -1,6 +1,6 @@
 #Adds a segment of DNA to an already-existing segment in a specific position that the user requires. If the user does not enter any position, the default position will be at the end of the already-existing strand. 
 def ligate(toBeLigated,sequence,index=None):
 	if index is not None:
-		return sequence[0:index] + toBeLigated + sequence[index:len(sequence)-1]
+		return sequence[0:index] + toBeLigated + sequence[index:len(sequence)]
 	else:
 		return sequence + toBeLigated
