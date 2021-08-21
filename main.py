0e51179ffcdf7d081ffe7f2bb078d5970561c050
Mohammed ayman <mae252002@gmail.com>
2021-08-21 22:53:48 +0200

    formatted the code properly

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from main_helper import *
 import analyze_strands
 
 
+# printing the program welcome message to the user
+
 def welcomeMsg():
     printMsgWithDelay(welcome_msg, 1)
     mainPrompt()
 
+
+# gets the required section from the user, directs to it, and handles the errors
 
 def mainPrompt(first=True):
     if first:
@@ -14,11 +18,12 @@ def mainPrompt(first=True):
     if not fetchInputError(main_prompt_choice, 'mainPrompt'):
         mainPrompt(False)
     else:
-        eval(main_prompt_options[main_prompt_choice] + ".main()")
+        eval(main_prompt_options[main_prompt_choice] + '.main()')
 
 
 def main():
     welcomeMsg()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
