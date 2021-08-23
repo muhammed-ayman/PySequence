c53fc42723a8959f8bb58d89c8095bdf752298b8
Mohammed ayman <mae252002@gmail.com>
2021-08-23 16:57:49 +0200

    edited the arithmetic operation in the percentage calculation

--- modules/sequencesMatchingPercentage.py
+++ modules/sequencesMatchingPercentage.py
@@ -2,7 +2,7 @@ from minimumEditDistance import minimum_edit_distance  # importing the minimum e
 
 
 def sequences_matching_percentage(seq_one, seq_two):  # getting two strings representing two sequences
-    match_per = (len(seq_one) + len(seq_two)
-                 - minimum_edit_distance(seq_one, seq_two)) * 100 \
-        / (len(seq_one) + len(seq_two))
+    edit_distance = minimum_edit_distance(seq_one, seq_two)
+    max_length = max(len(seq_one), len(seq_two))
+    match_per = 100*(1-edit_distance/max_length)
     return match_per  # returning the matching percentage between the two sequences
