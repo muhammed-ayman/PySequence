fd8f80c44d75367fd68f158c52498624c2c6e123
Mohammed ayman <mae252002@gmail.com>
2021-08-28 19:51:09 +0200

    added the complementary strand feature

--- single_analysis_gui.py
+++ single_analysis_gui.py
@@ -6,6 +6,86 @@ from tkinter import filedialog
 import analyze_gui
 from main_imports import *
 
+def complement():
+
+	#opening a window
+	window = Tk()
+
+	#file initiation
+	#def openFile():
+	#	window.filename = filedialog.askopenfilename(title="Choose file",filetypes=(("txt files","*.txt"),("All Types (*txt)","*.txt")))
+
+	#naming the title of the program
+	window.title("PySequence - Version 1.0 / Single Strand Analysis")
+
+	# Setting the geometry i.e Dimensions
+	window.geometry("750x500")
+
+	#disabling maximize and minimize buttons
+	window.resizable(0,0)
+
+	#changing background color
+	window.configure(background="white")
+
+	#font styling
+	nameFontStyle = tkFont.Font(family="Lucida Grande", size=20)
+
+	#defining labels
+	name = Label(window,text="PySequence 1.0",padx=20,pady=20,font=nameFontStyle,fg="dark red",bg="white").grid(row=0, column=0)
+	dnaInputLabel = Label(window, padx=20, pady=10, text="Enter Sequence",font="Helvetica 16 bold italic",fg="dark blue",bg="white")
+	dnaInputLabel.place(x=20,y=160)
+
+	#defining functions
+	def back():
+		window.destroy()
+		main()
+
+	def complement():
+		dna_seq = check_DNA_validity(dnaSeqInput.get())
+		if dna_seq == '':
+			user_response = messagebox.showwarning(title='No Sequence',message='Please Enter a Sequence!')
+			return
+		if not dna_seq:
+			user_response = messagebox.showwarning(title='Invalid Sequence',message='Invalid Sequence!')
+			return
+		feature_output = complement_DNA(dna_seq)
+		user_response = messagebox.showinfo(title='Complementary Strad',message=f'Your complementary strand is: {feature_output}')
+
+	#file initiation
+	def openFile():
+		window.filename = filedialog.askopenfilename(title="Choose file",filetypes=(("txt files","*.txt"),("All Types (*txt)","*.txt")))
+		return window.filename
+
+	def openDNAfile():
+		file_path = openFile()
+		if not os.path.isfile(file_path):
+			return
+		dna_file = open(file_path, 'r')
+		dna_seq = check_DNA_validity(dna_file.readline())
+		if not dna_seq:
+			user_response = messagebox.showwarning(title='Invalid Sequence',message='Invalid Sequence!')
+			return
+		dnaSeqInput.insert(END, dna_seq)
+
+	#defining image to be used as an icon for file navigation
+	fileImage = PhotoImage(file="Images/Folder_Icon_32.png")
+
+	#defining buttons
+	complementbtn = Button(window, relief="solid",borderwidth=4, padx=40, text="Complementary Strand",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=complement)
+	back = Button(window, relief="solid", borderwidth=4, padx=40, text="Back",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=back)
+	button_openFileDNA = Button(window,borderwidth=0,image=fileImage,command=openDNAfile,bg="white")
+
+	#displaying buttons
+	button_openFileDNA.place(x=250,y=220)
+	complementbtn.place(x=360,y=240)
+	back.place(x=360,y=300)
+
+	#defining input fields
+	dnaSeqInput = Entry(window,font="Helvetica 11 bold italic",bg="White",fg="dark blue",bd=3,relief="solid",width=25)
+	dnaSeqInput.place(x=20,y=220)
+
+	window.mainloop()
+
 def count_nuc():
 
 	#opening a window
@@ -126,10 +206,18 @@ def main():
 		window.destroy()
 		count_nuc()
 
+	def complement_local():
+		window.destroy()
+		complement()
+
+	def gc_content_local():
+		window.destroy()
+		gc_content()
+
 	#defining buttons
 	count = Button(window, relief="solid",borderwidth=4, padx=40, text="Count Nucleotides",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=count_nuc_local)
-	complement = Button(window, relief="solid",borderwidth=4, padx=40, text="Complementary Strand",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan")
-	gcContent = Button(window, relief="solid",borderwidth=4, padx=40, text="G-C Content",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan")
+	complementbtn = Button(window, relief="solid",borderwidth=4, padx=40, text="Complementary Strand",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=complement_local)
+	gcContent = Button(window, relief="solid",borderwidth=4, padx=40, text="G-C Content",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=gc_content_local)
 	palindrome = Button(window, relief="solid",borderwidth=4, padx=40, text="Palindrome?",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan")
 	ligate = Button(window, relief="solid",borderwidth=4, padx=40, text="Ligate a Segment",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan")
 	segmentSearch = Button(window, relief="solid",borderwidth=4, padx=40, text="Search for Segment",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan")
@@ -145,7 +233,7 @@ def main():
 
 	#displaying buttons
 	count.place(x=360,y=100)
-	complement.place(x=360,y=160)
+	complementbtn.place(x=360,y=160)
 	gcContent.place(x=360,y=220)
 	palindrome.place(x=360,y=280)
 	ligate.place(x=360,y=340)
