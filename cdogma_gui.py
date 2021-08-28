aa8f23c91e9588c5daa6c29dc7434c60db8df533
Mohammed ayman <mae252002@gmail.com>
2021-08-28 17:32:10 +0200

    finished central dogma gui

--- cdogma_gui.py
+++ cdogma_gui.py
@@ -5,83 +5,119 @@ from tkinter import messagebox
 from PIL import ImageTk, Image
 from tkinter import filedialog
 from main_imports import *
+from main_gui import main as main_gui_main
 
-#opening a window
-window = Tk()
+def main():
 
-#naming the title of the program
-window.title("PySequence - Version 1.0 / Central Dogma")
+	#opening a window
+	window = Tk()
 
-# Setting the geometry i.e Dimensions
-window.geometry("750x500")
+	#naming the title of the program
+	window.title("PySequence - Version 1.0 / Central Dogma")
 
-#disabling maximize and minimize buttons
-window.resizable(0,0)
+	# Setting the geometry i.e Dimensions
+	window.geometry("750x500")
 
-#changing background color
-window.configure(background="white")
+	#disabling maximize and minimize buttons
+	window.resizable(0,0)
 
-#file initiation
-def openFile():
-	window.filename = filedialog.askopenfilename(title="Choose file",filetypes=(("txt files","*.txt"),("All Types (*txt)","*.txt")))
-	return window.filename
+	#changing background color
+	window.configure(background="white")
 
-def openDNAfile():
-	file_path = openFile()
-	dna_file = open(file_path, 'r')
-	dna_seq = check_DNA_validity(dna_file.readline())
-	if not dna_seq:
-		user_response = messagebox.showwarning(title='Invalid Sequence',message='Invalid Sequence!')
-		return
-	dnaSeqInput.insert(END, dna_seq)
+	#file initiation
+	def openFile():
+		window.filename = filedialog.askopenfilename(title="Choose file",filetypes=(("txt files","*.txt"),("All Types (*txt)","*.txt")))
+		return window.filename
 
-def transcribeDNA():
-	dna_seq = check_DNA_validity(dnaSeqInput.get())
-	if not dna_seq:
-		user_response = messagebox.showwarning(title='Invalid Sequence',message='Invalid Sequence!')
-		return
-	user_response = messagebox.showinfo(title='Transcription Result',message=f'The transcribed DNA: {transcribe(dna_seq)}')
+	def openDNAfile():
+		file_path = openFile()
+		dna_file = open(file_path, 'r')
+		dna_seq = check_DNA_validity(dna_file.readline())
+		if not dna_seq:
+			user_response = messagebox.showwarning(title='Invalid Sequence',message='Invalid Sequence!')
+			return
+		dnaSeqInput.insert(END, dna_seq)
 
+	def transcribeDNA():
+		dna_seq = check_DNA_validity(dnaSeqInput.get())
+		if not dna_seq:
+			user_response = messagebox.showwarning(title='Invalid Sequence',message='Invalid Sequence!')
+			return
+		user_response = messagebox.showinfo(title='Transcription Result',message=f'The transcribed DNA: {transcribe(dna_seq)}')
 
-#font styling
-nameFontStyle = tkFont.Font(family="Lucida Grande", size=20)
+	def openRNAfile():
+		file_path = openFile()
+		rna_file = open(file_path, 'r')
+		rna_seq = check_RNA_validity(rna_file.readline())
+		if not rna_seq:
+			user_response = messagebox.showwarning(title='Invalid Sequence',message='Invalid Sequence!')
+			return
+		rnaSeqInput.insert(END, rna_seq)
 
-#defining labels
-name = Label(window,text="PySequence 1.0",padx=20,pady=20,font=nameFontStyle,fg="dark red",bg="white").grid(row=0,column=0)
-dnaInputLabel = Label(window, padx=20, pady=10, text="Your DNA Sequence",font="Helvetica 16 bold italic",fg="dark blue",bg="white")
-dnaInputLabel.place(x=0,y=160)
-rnaInputLabel = Label(window, padx=20, pady=10, text="Your RNA Sequence",font="Helvetica 16 bold italic",fg="dark blue",bg="white")
-rnaInputLabel.place(x=0,y=330)
+	def translateRNA():
+		rna_seq = check_RNA_validity(rnaSeqInput.get())
+		if not rna_seq:
+			user_response = messagebox.showwarning(title='Invalid Sequence',message='Invalid Sequence!')
+			return
+		rnaSequence_triplets = generate_triplets(rna_seq)
+		result = generate_polypeptides(rnaSequence_triplets)
+		if len(result) > 0:
+			user_response = messagebox.showinfo(title='Translation Result',message=f'Translation Output: {result}')
+		else:
+			user_response = messagebox.showinfo(title='Translation Result',message=f'Nothing to Translate!\nCheck your Sequence Start Codons')
 
-#defining functions
-def homepage():
-	window.destroy()
-	import main_gui
+	def automate_central_dogma():
+		dna_seq = check_DNA_validity(dnaSeqInput.get())
+		if not dna_seq:
+			user_response = messagebox.showwarning(title='Invalid Sequence',message='Invalid Sequence!')
+			return
+		transc_dna_seq = transcribe(dna_seq)
+		rnaSequence_triplets = generate_triplets(transc_dna_seq)
+		result = generate_polypeptides(rnaSequence_triplets)
+		if len(result) > 0:
+			user_response = messagebox.showinfo(title='Central Dogma Result',message=f'The Process Result: {result}')
+		else:
+			user_response = messagebox.showinfo(title='Translation Result',message=f'Nothing to Translate!\nCheck your Sequence Start Codons')
 
-#defining image to be used as an icon for file navigation
-fileImage = PhotoImage(file="Images/Folder_Icon_32.png")
+	#font styling
+	nameFontStyle = tkFont.Font(family="Lucida Grande", size=20)
 
-#defining buttons
-button_transcription = Button(window, relief="solid",borderwidth=4, padx=40, pady=20, text="Transcribe DNA",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=transcribeDNA)
-button_translation = Button(window, relief="solid", borderwidth=4, padx=40, pady=20, text="Translate RNA",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan")
-button_automation = Button(window, relief="solid", borderwidth=4, padx=40, pady=20, text="Automate Translation",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan")
-back = Button(window, relief="solid", borderwidth=4, padx=40, pady=20, text="Back",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=homepage)
-button_openFileDNA = Button(window,borderwidth=0,image=fileImage,command=openDNAfile,bg="white")
-button_openFileRNA = Button(window,borderwidth=0,image=fileImage,command=openFile,bg="white")
+	#defining labels
+	name = Label(window,text="PySequence 1.0",padx=20,pady=20,font=nameFontStyle,fg="dark red",bg="white").grid(row=0,column=0)
+	dnaInputLabel = Label(window, padx=20, pady=10, text="Your DNA Sequence",font="Helvetica 16 bold italic",fg="dark blue",bg="white")
+	dnaInputLabel.place(x=0,y=160)
+	rnaInputLabel = Label(window, padx=20, pady=10, text="Your RNA Sequence",font="Helvetica 16 bold italic",fg="dark blue",bg="white")
+	rnaInputLabel.place(x=0,y=330)
 
-#displaying buttons
-button_transcription.place(x=360,y=100)
-button_translation.place(x=360,y=190)
-button_automation.place(x=360,y=280)
-back.place(x=360,y=370)
-button_openFileDNA.place(x=250,y=220)
-button_openFileRNA.place(x=250,y=390)
+	#defining functions
+	def homepage():
+		window.destroy()
+		main_gui_main()
 
-#defining input fields
-dnaSeqInput = Entry(window,font="Helvetica 11 bold italic",bg="White",fg="dark blue",bd=3,relief="solid",width=25)
-dnaSeqInput.place(x=20,y=220)
-rnaSeqInput = Entry(window,font="Helvetica 11 bold italic",bg="White",fg="dark blue",bd=3,relief="solid",width=25)
-rnaSeqInput.place(x=20,y=390)
+	#defining image to be used as an icon for file navigation
+	fileImage = PhotoImage(file="Images/Folder_Icon_32.png")
 
-#running the program until the user clicks "X"
-window.mainloop()
+	#defining buttons
+	button_transcription = Button(window, relief="solid",borderwidth=4, padx=40, pady=20, text="Transcribe DNA",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=transcribeDNA)
+	button_translation = Button(window, relief="solid", borderwidth=4, padx=40, pady=20, text="Translate RNA",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=translateRNA)
+	button_automation = Button(window, relief="solid", borderwidth=4, padx=40, pady=20, text="Automate Central Dogma",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=automate_central_dogma)
+	back = Button(window, relief="solid", borderwidth=4, padx=40, pady=20, text="Back",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=homepage)
+	button_openFileDNA = Button(window,borderwidth=0,image=fileImage,command=openDNAfile,bg="white")
+	button_openFileRNA = Button(window,borderwidth=0,image=fileImage,command=openRNAfile,bg="white")
+
+	#displaying buttons
+	button_transcription.place(x=360,y=100)
+	button_translation.place(x=360,y=190)
+	button_automation.place(x=360,y=280)
+	back.place(x=360,y=370)
+	button_openFileDNA.place(x=250,y=220)
+	button_openFileRNA.place(x=250,y=390)
+
+	#defining input fields
+	dnaSeqInput = Entry(window,font="Helvetica 11 bold italic",bg="White",fg="dark blue",bd=3,relief="solid",width=25)
+	dnaSeqInput.place(x=20,y=220)
+	rnaSeqInput = Entry(window,font="Helvetica 11 bold italic",bg="White",fg="dark blue",bd=3,relief="solid",width=25)
+	rnaSeqInput.place(x=20,y=390)
+
+	#running the program until the user clicks "X"
+	window.mainloop()
--- main_gui.py
+++ main_gui.py
@@ -2,62 +2,68 @@
 from tkinter import *
 import tkinter.font as tkFont
 from tkinter import messagebox
+import cdogma_gui
 
-#opening a window
-window = Tk()
+def main():
 
-#naming the title of the program
-window.title("PySequence - Version 1.0")
+    #opening a window
+    window = Tk()
 
-# Setting the geometry i.e Dimensions
-window.geometry("750x500")
+    #naming the title of the program
+    window.title("PySequence - Version 1.0")
 
-#disabling maximize and minimize buttons
-window.resizable(0,0)
+    # Setting the geometry i.e Dimensions
+    window.geometry("750x500")
 
-#changing background color
-window.configure(background="white")
+    #disabling maximize and minimize buttons
+    window.resizable(0,0)
 
-#font styling
-nameFontStyle = tkFont.Font(family="Lucida Grande", size=20)
+    #changing background color
+    window.configure(background="white")
 
-#defining labels
-name = Label(window,text="PySequence 1.0",padx=20,pady=20,font=nameFontStyle,fg="dark red",bg="white")
+    #font styling
+    nameFontStyle = tkFont.Font(family="Lucida Grande", size=20)
 
-#defining function to swipe pages
-def manualPage():
-    window.destroy()
-    import manual_gui
+    #defining labels
+    name = Label(window,text="PySequence 1.0",padx=20,pady=20,font=nameFontStyle,fg="dark red",bg="white")
 
-def analyzepage():
-	window.destroy()
-	import analyze_gui
+    #defining function to swipe pages
+    def manualPage():
+        window.destroy()
+        import manual_gui
 
-def cdogmaepage():
-	window.destroy()
-	import cdogma_gui
+    def analyzepage():
+    	window.destroy()
+    	import analyze_gui
 
-def exitAssurance():
-	userResponse = messagebox.askyesno("Confirmation","Are you sure you want to exit?")
-	if userResponse == 1:
-		window.quit()
-	else:
-		return
+    def cdogmaepage():
+    	window.destroy()
+    	cdogma_gui.main()
 
-#defining buttons
-button_analyze = Button(window, relief="solid",borderwidth=4, padx=40, pady=20, text="Analyze",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=analyzepage)
-button_central_dogma = Button(window, relief="solid", borderwidth=4, padx=40, pady=20, text="Transcribe / Translate",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=cdogmaepage)
-button_manual = Button(window, relief="solid", borderwidth=4, padx=40, pady=20, text="Manual",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=manualPage)
-button_exit = Button(window, relief="solid", borderwidth=4, padx=40, pady=20, text="Exit",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=exitAssurance)
+    def exitAssurance():
+    	userResponse = messagebox.askyesno("Confirmation","Are you sure you want to exit?")
+    	if userResponse == 1:
+    		window.quit()
+    	else:
+    		return
 
-#displaying labels
-name.grid(row=0, column=0)
+    #defining buttons
+    button_analyze = Button(window, relief="solid",borderwidth=4, padx=40, pady=20, text="Analyze",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=analyzepage)
+    button_central_dogma = Button(window, relief="solid", borderwidth=4, padx=40, pady=20, text="Transcribe / Translate",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=cdogmaepage)
+    button_manual = Button(window, relief="solid", borderwidth=4, padx=40, pady=20, text="Manual",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=manualPage)
+    button_exit = Button(window, relief="solid", borderwidth=4, padx=40, pady=20, text="Exit",width=15,font="Helvetica 16 bold italic",fg="dark blue",bg="dark cyan",command=exitAssurance)
 
-#displaying buttons
-button_analyze.place(x=360,y=100)
-button_central_dogma.place(x=360,y=190)
-button_manual.place(x=360,y=280)
-button_exit.place(x=360,y=370)
+    #displaying labels
+    name.grid(row=0, column=0)
 
-#running the program until the user clicks "X"
-window.mainloop()
+    #displaying buttons
+    button_analyze.place(x=360,y=100)
+    button_central_dogma.place(x=360,y=190)
+    button_manual.place(x=360,y=280)
+    button_exit.place(x=360,y=370)
+
+    #running the program until the user clicks "X"
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
